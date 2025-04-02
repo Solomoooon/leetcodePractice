b32b286688f6565acf018eb7d8fc2e527b7359dd
Solomon Yu <slmyu0915@gmail.com>
2025-04-02 10:52:34 -0700

    Apr.2 Leetcode 199

--- Binary Tree/199.BinaryTreeRightSideView.py
+++ Binary Tree/199.BinaryTreeRightSideView.py
@@ -1,7 +1,25 @@
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+    if root is None:
+        return []
+    nextlevel = deque([root])
+    rightside = []
+    while nextlevel:
+        currlevel = nextlevel
+        nextlevel = deque()
+        while currlevel:
+            node = currlevel.popleft()
+            if node.left:
+                nextlevel.append(node.left)
+            if node.right:
+                nextlevel.append(node.right)
+        rightside.append(node.val)
