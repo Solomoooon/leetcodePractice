9156775ccfddc3ccb08ff999a65e4ffc3f82871b
Solomon Yu <slmyu0915@gmail.com>
2025-04-01 17:01:23 -0700

    updated previous questions

---
+++ Binary Tree/199.BinaryTreeRightSideView.py
@@ -0,0 +1,7 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
--- Binary Tree/94.BinaryTreeInOrderTraversal.py
+++ Binary Tree/94.BinaryTreeInOrderTraversal.py
@@ -5,23 +5,21 @@ class TreeNode:
         self.right = right
 
 
-# in order traversal is same to sorted list, so the kth smallest is the kth element for in order traversal
-def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-    # array for storing in order traversal
-    nums = []
+def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    if root == None:
+        return []
+    cur = root
+    results = []
 
-    # in order traversal saves in only two circumstances:
-    # when a node has no more leafs and if it finishes traversing left or has no left child
-    def traverse(cur, num):
+    def traverse(cur):
         if cur.left == None and cur.right == None:
-            nums.append(cur.val)
+            results.append(cur.val)
             return
         if cur.left:
-            traverse(cur.left, num)
-        nums.append(cur.val)
+            traverse(cur.left)
+        results.append(cur.val)
         if cur.right:
-            traverse(cur.right, num)
+            traverse(cur.right)
 
-    traverse(root, 0)
-    # With the nums recording the entire sorted tree list, we return the k-1 element as the kth smallest
-    return nums[k - 1]
+    traverse(cur)
+    return results
