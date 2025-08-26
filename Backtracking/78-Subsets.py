e4541d2253eb1cd13aab779d6f8706efe7898710
Solomon Yu <slmyu0915@gmail.com>
2025-08-26 17:37:20 -0500

    Leetcode 78 new comments and 3000

--- Backtracking/78-Subsets.py
+++ Backtracking/78-Subsets.py
@@ -2,13 +2,15 @@ def subsets(self, nums: List[int]) -> List[List[int]]:
     results = []
 
     def backtrack(start, comb):
-        results.append(comb[:])
+        results.append(list(comb))
+        # 第一种情况是只包含到start目前为止的一个subset
 
         for i in range(start, len(nums)):
             comb.append(nums[i])
+            # 第二种是包含nums[i]的情况
             backtrack(i + 1, comb)
             comb.pop()
+            # 第三种是不包含nums[i]的情况，随着i不断更新start的值，添加各种不包含nums[i]的情况
 
-    comb = []
-    backtrack(0, comb)
+    backtrack(0, [])
     return results
