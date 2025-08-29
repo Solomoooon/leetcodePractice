86b5437f3e994741c0aa17ff25d2a799ce9422cc
Solomon Yu <slmyu0915@gmail.com>
2025-08-29 15:11:13 -0500

    Leetcode 79

--- Backtracking/79-WordSearch.py
+++ Backtracking/79-WordSearch.py
@@ -1,42 +1,38 @@
-from typing import List
-
-
-def exist(board: List[List[str]], word: str) -> bool:
-    if len(word) == 0:
-        return True
-
-    def backtrack(index, row, col):
-        if index == len(word):
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        if len(word) == 0:
             return True
 
-        if (
-            row < 0
-            or row >= len(board)
-            or col < 0
-            or col >= len(board[0])
-            or word[index] != board[row][col]
-        ):
-            return False
-
-        recover = board[row][col]
-        board[row][col] = -1
-
-        for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            if backtrack(index + 1, dr, dc):
+        def backtrack(index, row, col):
+            if index == len(word):
                 return True
 
-        board[row][col] = recover
-        return False
+            if (
+                row < 0
+                or row > len(board) - 1
+                or col < 0
+                or col > len(board[0]) - 1
+                or word[index] != board[row][col]
+            ):
+                return False
 
-    for i in range(0, len(board)):
-        for j in range(0, len(board[0])):
-            if board[i][j] == word[0]:
-                if backtrack(0, i, j):
+            recover = board[row][col]
+
+            board[row][col] = -1
+
+            for dr, dc in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                if backtrack(index + 1, row + dr, col + dc):
                     return True
 
-    return False
+            board[row][col] = recover
+            return False
 
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0]:
+                    print("aa")
+                    if backtrack(0, i, j):
+                        print("aa")
+                        return True
 
-testboard = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
-testword = "ABCCED"
-print(exist(testboard, testword))
+        return False
